1224de30e11c1f1fc4dae71ac264036f7a496c81
bob sagat <wiiqwertyuiop@gmail.com>
2023-10-19 21:33:01 -0400

    update

--- src/web/typescript/out/code.js
+++ src/web/typescript/out/code.js
@@ -1,5 +1,5 @@
 async function test() {
-    const raw = await fetch('https://us-east1-wiiq-proj.cloudfunctions.net/hello', {
+    const raw = await fetch('https://us-central1-wiiq-proj.cloudfunctions.net/on_request_example', {
         method: 'GET',
     });
     console.log(raw);
